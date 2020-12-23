4f22cf526825747ca78c45634a65f16b028c2ff5
Humberto Diógenes <hdiogenes@gmail.com>
2020-12-23 13:10:38 -0300

    Version bump to 1.1 (#58)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if 'bdist_wheel' in sys.argv:
 BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
 
 NAPP_NAME = 'of_lldp'
-NAPP_VERSION = '1.0'
+NAPP_VERSION = '1.1'
 
 # Kytos var folder
 VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
